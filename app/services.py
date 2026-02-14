d1ebeb67eac53415cfb05bf48f30c0589a146c0c
keoniakina <kcakina@gmail.com>
2026-02-14 12:12:44 -0800

    business logic

---
+++ app/services.py
@@ -0,0 +1,11 @@
+from app.db import DB, TINY_URL
+from typing import Optional
+
+def assign_short_url(db: DB, long_url: str, count: int) -> Optional[TINY_URL]:
+    try:
+        new_url = TINY_URL(long_url, url_id=count)
+        db.set_long_and_short(new_url)
+        count += 1
+        return new_url
+    except ValueError:
+        return None
